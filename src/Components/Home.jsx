1f5ecf1de1ff39ecf401cdd26a29bea778fbe095
Pramod Pandey <61660719+ppandey895@users.noreply.github.com>
2022-09-07 21:10:40 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,6 @@ import Header from './Header';
 import Interests from './Interests';
 import Projects from './Projects';
 import About from './About';
-import Contact from './Contact';
 
 const Home = props => {
 	const [navSelected, setNav] = useState('header');
@@ -14,7 +13,6 @@ const Home = props => {
 			{ navSelected === 'interests' ? <Interests setNav={setNav} /> : "" }
 			{ navSelected === 'projects' ? <Projects  setNav={setNav} /> : "" }
 			{ navSelected === 'about me' ? <About setNav={setNav} /> : "" }
-			{ navSelected === 'contact' ? <Contact setNav={setNav} /> : "" }
 		</div>
 	)
 }
